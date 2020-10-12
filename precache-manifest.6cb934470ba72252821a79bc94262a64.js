92118e8649924d2b5e7fad3b394e6ef2591491b7
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-10-12 09:25:11 +0000

    Deploy PatilMugdha/portfolio to github.com/PatilMugdha/portfolio.git:gh-pages

---
+++ precache-manifest.6cb934470ba72252821a79bc94262a64.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "918f3de41d1dfbe0941c",
+    "url": "/portfolio/static/css/main.a12533ea.chunk.css"
+  },
+  {
+    "revision": "918f3de41d1dfbe0941c",
+    "url": "/portfolio/static/js/main.918f3de4.chunk.js"
+  },
+  {
+    "revision": "0fad4c0f10d57b530fe4",
+    "url": "/portfolio/static/js/runtime~main.0fad4c0f.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/portfolio/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "071222da0a06b004d3312718ac63a288",
+    "url": "/portfolio/index.html"
+  }
+];
